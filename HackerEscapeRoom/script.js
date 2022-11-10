5541e160501bda5e8232f44c86f0be577df78b0f
Yvette <nchombuayvta@gmil.com>
2022-11-10 12:26:38 +0100

    mobile menu fixed

--- HackerEscapeRoom/script.js
+++ HackerEscapeRoom/script.js
@@ -1,2 +1,11 @@
-const button = document.querySelector('.navbar-toggler-icon')
-console.log('parent', button.parentElement)
+
+const navMenu = document.querySelector('#hamburger-icon')
+const navList = document.querySelector('.navbarNav')
+
+navMenu.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+    navMenu.classList.toggle('open')
+    navList.classList.toggle('navbarNav')
+}
+
