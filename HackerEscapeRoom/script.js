75c576ba9de9e98f3bdd1a4a8e51b03e5c3e4e57
Yvette <nchombuayvta@gmil.com>
2022-11-11 21:37:33 +0100

    changed some styles

--- HackerEscapeRoom/script.js
+++ HackerEscapeRoom/script.js
@@ -1,14 +1,25 @@
 
 const hamburger = document.querySelector('#hamburger-icon')
-const navMenu = document.querySelector('.navbarNav')
+const navMenu = document.querySelector('.nav-menu')
+const closeBtn = document.querySelector('.fa-xmark')
+console.log('nav', navMenu)
+console.log('close', closeBtn)
 
 
-hamburger.addEventListener('click', toggleMenu)
+hamburger.addEventListener('click', openMenu)
+closeBtn.addEventListener('click', closeMenu)
 
-function toggleMenu() {
-    hamburger.classList.toggle('open')
-    navMenu.classList.toggle('navbarNav')
+function openMenu() {
+    // hamburger.classList.toggle('open')
+    navMenu.classList.add('nav-menu')
+    // closeBtn.style.display = 'block'
 
+}
+
+function closeMenu() {
+    // hamburger.classList.toggle('open')
+    navMenu.classList.remove('nav-menu')
+    closeBtn.style.display = 'none'
 
 }
 
