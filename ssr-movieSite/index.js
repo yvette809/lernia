bd1eeafc1a9dd4ca742d37416dae836f218bc1f2
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-01-24 20:06:52 +0100

    pages added

--- ssr-movieSite/index.js
+++ ssr-movieSite/index.js
@@ -1,11 +1,9 @@
-import express from "express"
-import {engine} from "express-handlebars"
-import {marked} from "marked"
-import { getMovies, getMovie } from "./src/movies.js"
+import express from "express";
+import { engine } from "express-handlebars";
+import { marked } from "marked";
+import { getMovies, getMovie } from "./src/movies.js";
 
-
-
-const app = express()
+const app = express();
 
 /* app.engine("handlebars", engine({
     helpers: {
@@ -13,33 +11,70 @@ const app = express()
     },
   })); */
 
-  app.engine('handlebars', engine())
-// set our app to use the handlebars engine
-app.set("view engine", "handlebars")
-app.set("views", "./templates")
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
 
-app.get("/", async(req,res)=>{
-  const moviesFromApi = await getMovies()
-res.render("home", {moviesFromApi})
- 
-})
+const MENU = [
+  {
+    label: "Öppetider & Kontakt",
+    link: "/openinghours",
+  },
+  {
+    label: "Om Spegeln",
+    link: "/about",
+  },
+  {
+    label: "Biljettinfo",
+    link: "/ticket-info",
+  },
+  {
+    label: "Vill du har vårt nyhetsbrev?",
+    link: "/newsletter",
+  },
+  {
+    label: "Köp Presentkort",
+    link: "/giftcard",
+  },
+];
 
-app.get("/movies/:movieId", async(req,res)=>{
+app.get("/", async (req, res) => {
+  const moviesFromApi = await getMovies();
+  res.render("home", { moviesFromApi });
+});
 
-  const singleMovie = await getMovie(req.params.movieId)
-  console.log('singleMovie', singleMovie)
-  if(singleMovie){
-    res.render("movie",{singleMovie})
-  }else{
-    res.status(404).render("404")
+app.get("/movies/:movieId", async (req, res) => {
+  const singleMovie = await getMovie(req.params.movieId);
+  if (singleMovie) {
+    res.render("movie", { singleMovie });
+  } else {
+    res.status(404).render("404");
   }
- 
+});
 
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-app.use("/static", express.static("./static"))
+app.get("/ticket-info", (req, res) => {
+  res.render("ticket");
+});
 
-const port = 5080
-app.listen(port, ()=>{
-  console.log(`server is running on port ${port}`)
-})
+app.get("/newsletter", (req, res) => {
+  res.render("news");
+});
+
+app.get("/giftcard", (req, res) => {
+  res.render("giftcard");
+});
+
+app.get("/openinghours", (req, res) => {
+  res.render("contact");
+});
+
+app.use("/static", express.static("./static"));
+
+const port = 5080;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
