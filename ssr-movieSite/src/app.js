83b683a24b5f4df8d457f9a17469819c96daa579
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-01-26 11:58:16 +0100

    test added

---
+++ ssr-movieSite/src/app.js
@@ -0,0 +1,77 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import { marked } from "marked";
+import { getMovies, getMovie } from "../src/movies.js";
+
+const app = express();
+
+/* app.engine("handlebars", engine({
+    helpers: {
+      markdown: md => marked(md),
+    },
+  })); */
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
+
+const MENU = [
+  {
+    label: "Öppetider & Kontakt",
+    link: "/openinghours",
+  },
+  {
+    label: "Om Spegeln",
+    link: "/about",
+  },
+  {
+    label: "Biljettinfo",
+    link: "/ticket-info",
+  },
+  {
+    label: "Vill du har vårt nyhetsbrev?",
+    link: "/newsletter",
+  },
+  {
+    label: "Köp Presentkort",
+    link: "/giftcard",
+  },
+];
+
+app.get("/", async (req, res) => {
+  const moviesFromApi = await getMovies();
+  res.render("home", { moviesFromApi });
+});
+
+app.get("/movies/:movieId", async (req, res) => {
+  const singleMovie = await getMovie(req.params.movieId);
+  if (singleMovie) {
+    res.render("movie", { singleMovie });
+  } else {
+    res.status(404).render("404");
+  }
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/ticket-info", (req, res) => {
+  res.render("ticket");
+});
+
+app.get("/newsletter", (req, res) => {
+  res.render("news");
+});
+
+app.get("/giftcard", (req, res) => {
+  res.render("giftcard");
+});
+
+app.get("/openinghours", (req, res) => {
+  res.render("contact");
+});
+
+app.use("/static", express.static("./static"));
+
+export default app
---
+++ ssr-movieSite/tests/test.js
@@ -0,0 +1,27 @@
+import app from "../src/app.js";
+import request from "supertest";
+import { getMovie } from "../src/movies.js";
+
+/* test('movie page contains title', async () => {
+    const response = await request(app)
+      .get('/movies/1')
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect(200);
+  
+    expect(response.text.includes('Isle of dogs')).toBeTruthy();
+  
+  }); */
+
+test("movie page contains title", async () => {
+  const id = 2;
+  const response = await request(app)
+    .get("/movies/" + id)
+    .expect("Content-Type", "text/html; charset=utf-8")
+    .expect(200);
+
+  const apiResponse = await getMovie(id);
+
+  //expect(response.text).toMatch(apiResponse.attributes.title);
+
+  expect(response.text.includes(apiResponse.attributes.title)).toBeTruthy();
+});
