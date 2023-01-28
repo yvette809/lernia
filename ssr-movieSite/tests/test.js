6ddf11cd168e4f070e2b88d9b29db6a205c86997
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-01-28 22:35:22 +0100

    test updated

--- ssr-movieSite/tests/test.js
+++ ssr-movieSite/tests/test.js
@@ -1,16 +1,15 @@
 import app from "../src/app.js";
 import request from "supertest";
-import { getMovie } from "../src/movies.js";
+import { getMovie, getMovies } from "../src/movies.js";
 
-/* test('movie page contains title', async () => {
-    const response = await request(app)
-      .get('/movies/1')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(200);
-  
-    expect(response.text.includes('Isle of dogs')).toBeTruthy();
-  
-  }); */
+test("Isles of Dog page containins dog info", async () => {
+  const response = await request(app)
+    .get("/movies/1")
+    .expect("Content-Type", "text/html; charset=utf-8")
+    .expect(200);
+
+  expect(response.text.includes("Isle of dogs")).toBeTruthy();
+});
 
 test("movie page contains title", async () => {
   const id = 2;
@@ -24,4 +23,14 @@ test("movie page contains title", async () => {
   //expect(response.text).toMatch(apiResponse.attributes.title);
 
   expect(response.text.includes(apiResponse.attributes.title)).toBeTruthy();
+});
+
+test("hompage shows list of movies", async () => {
+  const response = await request(app)
+    .get("/")
+    .expect(200)
+    .expect("Content-Type", "text/html; charset=utf-8");
+
+
+  expect(response.text.includes('Shawshank')).toBeTruthy();
 });
