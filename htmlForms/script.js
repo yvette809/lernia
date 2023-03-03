45de32d898664f2921a4b2d6538cc75b9b14b639
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-03-03 12:13:20 +0100

    registration

---
+++ htmlForms/script.js
@@ -0,0 +1,125 @@
+let form = document.querySelector("form");
+let name = document.querySelector("#name");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let errrorDiv = document.querySelectorAll(".error");
+
+// show input error mesage
+function showError(input, message) {
+  const parent = input.parentElement;
+  parent.classList.add("error");
+  parent.classList.remove("success");
+  const small = parent.querySelector("small");
+  small.innerText = message;
+}
+
+// show success message
+function showSuccess(input) {
+  const parent = input.parentElement;
+  parent.classList.remove("error");
+  parent.classList.add("success");
+}
+
+// check required fields
+
+const checkRequired = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${input.id} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+// check email validity
+const validateEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(String(email).toLowerCase());
+};
+
+// show Alert
+function showAlert(message, className) {
+  const div = document.createElement("div");
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector(".container");
+  container.insertBefore(div, form);
+
+  //disappear in 3 seconds
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 3000);
+}
+
+// register user
+const register = (name, email, password) => {
+  let users = [];
+  let user = {
+    id: Date.now().toString(),
+    name,
+    email,
+    password,
+  };
+
+  users.unshift(user);
+  showAlert("user added", "success");
+  localStorage.setItem("user", JSON.stringify(user));
+  console.log("users", users);
+};
+
+//login user
+
+const logIn = (email, password) => {
+  console.log(email, password);
+  showAlert("user loggedIn", "success");
+  location.replace("http://127.0.0.1:5501/htmlForms/home.html");
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (location.href === "http://127.0.0.1:5501/htmlForms/index.html") {
+    checkRequired([name, email, password]);
+    if (
+      name.value !== "" &&
+      email.value !== "" &&
+      password.value !== "" &&
+      validateEmail(email.value)
+    ) {
+      register(name.value, email.value, password.value);
+
+      name.value = "";
+      email.value = "";
+      password.value = "";
+    }
+  } else {
+    checkRequired([email, password]);
+    if (
+      email.value !== "" &&
+      password.value !== "" &&
+      validateEmail(email.value)
+    ) {
+      logIn(email.value, password.value);
+      email.value = "";
+      password.value = "";
+    }
+  }
+});
+
+// get user
+
+function getUser() {
+    if(location.href === 'http://127.0.0.1:5501/htmlForms/home.html'){
+        let user = localStorage.getItem("user");
+        console.log("user", user);
+        let username = document.querySelector(".user");
+        console.log("username", username);
+        username.innerText = `Welcome ${user.name}`;
+    }
+ 
+}
+
+console.log("getUser", getUser());
+//window.addEventListener("DOMContentLoaded", getUser);
--- ssr-movieSite/src/app.js
+++ ssr-movieSite/src/app.js
@@ -1,15 +1,9 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import { marked } from "marked";
-import { getMovies, getMovie } from "../src/movies.js";
+import { getMovies, getMovie, getScreenings } from "../src/movies.js";
 
 const app = express();
-
-/* app.engine("handlebars", engine({
-    helpers: {
-      markdown: md => marked(md),
-    },
-  })); */
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
@@ -36,6 +30,11 @@ const MENU = [
     label: "Köp Presentkort",
     link: "/giftcard",
   },
+
+  {
+    label: "screens",
+    link: "/screen",
+  },
 ];
 
 app.get("/", async (req, res) => {
@@ -50,6 +49,20 @@ app.get("/movies/:movieId", async (req, res) => {
   } else {
     res.status(404).render("404");
   }
+});
+
+app.get("/screenings", async (req, res) => {
+  const movieScreenings = await getScreenings();
+  const finalScreens = movieScreenings.map((screen) => {
+    return {
+      id: screen.id,
+      title: screen.title,
+      startTime: screen.startTime.toString().split("T")[0],
+      time: screen.startTime.toString().split("T")[1].slice(0, 5),
+    };
+  });
+  // const { id, title, startTime } = movieScreenings;
+  res.render("screens", { finalScreens });
 });
 
 app.get("/about", (req, res) => {
@@ -72,11 +85,10 @@ app.get("/openinghours", (req, res) => {
   res.render("contact");
 });
 
-
 app.use("/static", express.static("./static"));
 
-app.get('*', (req,res)=>{
-  res.render("404", {message: 'Page not found'})
-})
+app.get("*", (req, res) => {
+  res.render("404", { message: "Page not found" });
+});
 
-export default app
+export default app;
